6edfbeb2a93f0e6338ce50f42e3ba19decd28cca
AbhiDroid <abhinabroy2001@gmail.com>
2021-03-14 19:06:35 +0530

    making a new home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthService } from './auth/auth.service';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SignupComponent,
-    AuthService
+    AuthService,
+    HomeComponent
   ],
   imports: [
     BrowserModule
