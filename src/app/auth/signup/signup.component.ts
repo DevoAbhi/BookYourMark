711f2322c82767adee127691a87ea66c9c328ea6
AbhiDroid <abhinabroy2001@gmail.com>
2021-03-15 06:49:10 +0530

    Add submit function for passing values to the backend server

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { iif } from 'rxjs';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -11,14 +13,41 @@ export class SignupComponent implements OnInit {
 
 
   form: FormGroup;
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, public router: Router) { }
 
   ngOnInit(): void {
-
     this.form = new FormGroup({
-      'username' : new FormControl(null, {validators})
+      'username' : new FormControl(null, {validators: [Validators.required, Validators.minLength(5)]}),
+      'email': new FormControl(null, {validators: [Validators.required, Validators.email]}),
+      'password': new FormControl(null, {validators: [Validators.required, Validators.minLength(6)]}),
+      'confirmPassword': new FormControl(null, {validators: [Validators.required, Validators.minLength(6)]})
     })
   }
 
-  onSubmit(){}
+  onSubmit(){
+    if(this.form.invalid){
+      console.log("hello")
+      return
+    }
+    console.log(this.form.value)
+    const username = this.form.value.username;
+    const email = this.form.value.email;
+    const password = this.form.value.password;
+    const confirmPassword = this.form.value.confirmPassword;
+
+    if(password != confirmPassword){
+      console.log('Confirm Password is not same as Password');
+      this.form.reset();
+      return
+    }
+
+    console.log(username);
+    console.log(email);
+    console.log(password)
+    this.authService.signup(username, email, password)
+
+    this.form.reset();
+
+    this.router.navigate(['/']);
+  }
 }
