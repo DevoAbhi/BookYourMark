0de037f062e9454bd6e6c389878506fc517ee40b
AbhiDroid <abhinabroy2001@gmail.com>
2021-03-18 10:07:33 +0530

    Check if the user is authenticated and reflect it in nav bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit, OnDestroy {
+  isUserAuthenticated: boolean;
+  isAuthSub: Subscription;
 
-  ngOnInit(){
+  constructor(private authService: AuthService) {}
+  ngOnInit() {
+    this.isAuthSub = this.authService
+      .getAuthStatusListener()
+      .subscribe((isAuth) => {
+        this.isUserAuthenticated = isAuth;
+      });
   }
 
+  ngOnDestroy() {
+    this.isAuthSub.unsubscribe();
+  }
 }
