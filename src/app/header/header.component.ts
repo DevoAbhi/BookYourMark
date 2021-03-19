5e43b87ae6e81286d5a3bb8436257658bbd45ec1
AbhiDroid <abhinabroy2001@gmail.com>
2021-03-20 04:13:30 +0530

    Add logout button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,15 +8,19 @@ import { AuthService } from '../auth/auth.service';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-  isUserAuthenticated: boolean;
+  isUserAuthenticated = false;
   isAuthSub: Subscription;
+  
 
   constructor(private authService: AuthService) {}
   ngOnInit() {
+    console.log(this.authService.getIsAuthenticated())
+    console.log("Hello madharchod bsdk")
+    this.isUserAuthenticated = this.authService.getIsAuthenticated();
     this.isAuthSub = this.authService
       .getAuthStatusListener()
       .subscribe((isAuth) => {
-        this.isUserAuthenticated = isAuth;
+        // this.isUserAuthenticated = isAuth;
       });
   }
 
